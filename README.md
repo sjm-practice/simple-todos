5826f207f7da547620e71611f833cc8bc5fce478
stevenjmarsh <sjm.atl@gmail.com>
2015-07-01 16:30:04 -0700

    adding style

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # simple-todos
-The meteor simple-todos app tutorial.
+The meteor simple-todos app tutorial. This tutorial was completed as a code-along.
+
+## Notes
+* ...
+
--- simple-todos.js
+++ simple-todos.js
@@ -1,10 +1,10 @@
 if (Meteor.isClient) {
 
-  Template.body.helpers({
-    tasks: [
-      { text: "Task 1"},
-      { text: "Task 2"},
-      { text: "Task 3"}
-    ]
-  });
+    Template.body.helpers({
+        tasks: [
+            {text: "Task 1"},
+            {text: "Task 2"},
+            {text: "Task 3"}
+        ]
+    });
 }
