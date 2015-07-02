08f902a70a1b67312c14409d92343b5de62a0bb9
stevenjmarsh <sjm.atl@gmail.com>
2015-07-02 16:50:05 -0700

    delete and checked

--- simple-todos.js
+++ simple-todos.js
@@ -22,4 +22,13 @@ if (Meteor.isClient) {
             return false;     // signals browser to not do default action
         }
     });
+
+    Template.task.events({
+        "click .toggle-checked": function () {
+            Tasks.update(this._id, {$set: {checked: !this.checked}});
+        },
+        "click .delete": function () {
+            Tasks.remove(this._id);
+        }
+    });
 }
