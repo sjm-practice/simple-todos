dd06126be826bc88d9c154707d8059d3b481cab7
stevenjmarsh <sjm.atl@gmail.com>
2015-07-02 17:34:33 -0700

    hide completed

--- simple-todos.js
+++ simple-todos.js
@@ -4,7 +4,14 @@ if (Meteor.isClient) {
 
     Template.body.helpers({
         tasks: function () {
-            return Tasks.find({}, { sort: { createdAt: -1 } });
+            if (Session.get("hideCompleted")) {
+                return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}})
+            } else {
+                return Tasks.find({}, {sort: {createdAt: -1}})
+            }
+        },
+        hideCompleted: function () {
+            return Session.get("hideComplete");
         }
     });
 
@@ -20,6 +27,9 @@ if (Meteor.isClient) {
             event.target.text.value = "";
 
             return false;     // signals browser to not do default action
+        },
+        "change .hide-completed input": function (event) {
+            Session.set("hideCompleted", event.target.checked);
         }
     });
 
